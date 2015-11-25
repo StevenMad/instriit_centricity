e5240ae2ff503ad98ca512de8d67b35143f3bacf
thiery ouattara <thiery@debian>
2015-11-25 06:55:42 +0000

    marquer avec image

--- js/manip.js
+++ js/manip.js
@@ -2,6 +2,19 @@ window.onload = function(){
     var accessToken = "pk.eyJ1IjoiaXNoaWp1IiwiYSI6ImNpaGRpNXh3MTAwMm11OW00Mmd5NHE1bXkifQ.5VJtYHibmT3X-aEQSKz1Ag";
     var map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token='+accessToken).addTo(map);
+    var LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: 'http://wikipics.net/photos/20150125142221651988185.jpg',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
+    var greenIcon = new LeafIcon({iconUrl: 'http://wikipics.net/photos/20150125142221651988185.jpg'});
+    L.marker([48.889579, 2.320540], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+    
     L.marker([48.810718, 2.384341]).addTo(map) .bindPopup("<b>Thiery ici</b>");
     L.marker([48.831366, 2.355818]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
     L.marker([48.831405, 2.355545]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
@@ -12,3 +25,4 @@ window.onload = function(){
     L.marker([48.892965, 2.320154]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
     L.marker([48.715685, 2.041376]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
 }
+

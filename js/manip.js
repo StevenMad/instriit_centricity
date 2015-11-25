8726eba584864d7bbdb373aaa9a011309429e555
StevenMad <steven-madhow@outlook.fr>
2015-11-25 02:21:34 +0100

    js/manip.js mis a jour

--- js/manip.js
+++ js/manip.js
@@ -1,10 +1,7 @@
 window.onload = function(){
     var accessToken = "pk.eyJ1IjoiaXNoaWp1IiwiYSI6ImNpaGRpNXh3MTAwMm11OW00Mmd5NHE1bXkifQ.5VJtYHibmT3X-aEQSKz1Ag";
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([48.852966, 2.349902], 14);
     L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token='+accessToken).addTo(map);
-<<<<<<< HEAD
-
-=======
     L.marker([48.810718, 2.384341]).addTo(map) .bindPopup("<b>Thiery ici</b>");
     L.marker([48.831366, 2.355818]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
     L.marker([48.831405, 2.355545]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
@@ -14,5 +11,4 @@ window.onload = function(){
     L.marker([48.865871, 2.416285]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
     L.marker([48.892965, 2.320154]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
     L.marker([48.715685, 2.041376]).addTo(map) .bindPopup("<b>Henri ici</b>"); 
->>>>>>> e6c7153bc6ac82b546c1c5fda0082661cc9d0238
 }
